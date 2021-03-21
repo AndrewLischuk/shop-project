12cde1ca5419099a27ac5c695e6b351fd65626a3
MarshallGunn <marshallgunn59@gmail.com>
2021-03-21 11:48:44 +0200

    Added products quantuty

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 
-//===========PRODUCT COUNTER (CART)===============
+//===============PRODUCT COUNTER (CART)===============
 
 let productsCountEl = document.getElementById("products-counter")
 
@@ -12,7 +12,7 @@ for(let i = 0; i < addToCartBtns.length; i++) {
     })
 }
 
-//===========IS PRODUCT ICONS CHECKED===============
+//===============IS PRODUCT ICONS CHECKED===============
 
 let imgCheckIconsEl = document.querySelectorAll(".check-icons div");
 
@@ -42,4 +42,60 @@ for (let i = 0; i < imgCheckIconsEl.length; i++) {
             }
         }        
     })
+}
+
+//===============MORE DETAILS FORM===============
+
+let detailsBtn = document.querySelectorAll(".details");
+let modalForm = document.querySelector(".modal");
+let closeModalForm = document.querySelector(".btn-close");
+
+detailsBtn.forEach(btn => {
+    btn.addEventListener("click", openModal)
+})
+
+closeModalForm.addEventListener("click", hideModal)
+
+function openModal() {
+    modalForm.classList.remove("hide");
+    modalForm.classList.add("show");
+}
+
+function hideModal() {
+    modalForm.classList.add("hide");
+    modalForm.classList.remove("show");
+}
+
+modalForm.addEventListener("click", function(e) {
+    if(e.target === modalForm) {
+        hideModal()
+    }
+})
+//===============MORE DETAILS FORM SCROLL===============
+
+function showModalByScroll() {
+    if(window.pageYOffset > document.body.scrollHeight/2){
+        openModal();
+        window.removeEventListener("scroll", showModalByScroll)
+    }
+}
+window.addEventListener("scroll", showModalByScroll)
+
+//===============PRODUCTS QUANTITY===============
+
+let quantityBtn = document.querySelectorAll(".quantity-btn");
+let quantityField = document.querySelectorAll(".products-quantity input");
+
+for (let i = 0; i < quantityBtn.length; i++) {
+    if (i % 2 === 0) {        
+        quantityBtn[i].addEventListener("click", function() {
+            if (quantityField[Math.floor(i/2)].value > 1) {
+               quantityField[Math.floor(i/2)].value = +quantityField[Math.floor(i/2)].value - 1;
+            }            
+        })
+    } else {
+        quantityBtn[i].addEventListener("click", function() {
+            quantityField[Math.floor(i/2)].value = +quantityField[Math.floor(i/2)].value + 1;
+        })
+    }
 }
